b601f7ccad771e57adcfcab7f1eedc5eafe0a0ae
dtassoni <dtassoni01@gmail.com>
2024-03-04 20:34:34 -0500

    added an X^2 transformation example

---
+++ Regression/x^2_transformation_example.R
@@ -0,0 +1,52 @@
+library(openxlsx)
+setwd("C:/Users/dtass/OneDrive/Desktop/R work")
+#reads data
+D = read.xlsx(xlsxFile= "box_cox_example.xlsx")
+D
+
+#plots data
+plot(D$x , D$y)
+
+#makes a model
+
+model = lm(D$y ~ D$x)
+summary(model)
+#saves res
+RES = model$residuals
+#plots residuals to check for pattern
+plot(D$x , RES)
+
+#does qq plot
+qqnorm(RES)
+qqline(RES)
+
+#shaprio test
+
+shapiro.test(RES)
+# makes new model
+model2 = lm(D$y ~ D$x + I(D$x^2))
+summary(model2)
+
+# re saves res
+RES2 = model2$residuals
+
+#new qq plot
+qqnorm(RES2)
+qqline(RES2)
+
+#redoes sharpiro test
+shapiro.test(RES2)
+
+#plots res looking for patternn
+plot(D$x , RES2)
+#sees no pattern and is random
+
+# plots data with new model curve
+plot(D$x , D$y)
+curve(expr = model2$coefficients[1] +
+        model2$coefficients[2]*x +
+        model2$coefficients[3]*x^2, col = "red", lty =
+        "solid", lwd = 1, add = TRUE)
+        
+
+#much better linear
